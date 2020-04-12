1dc5f074c03365ee707be20ad53baf8a06920e33
dpadillaf <dpadillafonseca@gmail.com>
2020-04-12 14:18:58 -0500

    basic api rest

---
+++ products.py
@@ -0,0 +1,7 @@
+#data
+
+products = [
+    { "name": "laptop", "price": 800, "quantity": 16 },
+    { "name": "mouse", "price": 90, "quantity": 22 },
+    { "name": "monitor", "price": 150, "quantity": 18 }
+]
